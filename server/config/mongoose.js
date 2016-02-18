1efe4b61c5910dff64337b33e608f3aa255598d5
Jordan Blaine <jordanblaine2271@gmail.com>
2016-02-18 03:02:24 -0800

    Mongoose Update

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://0.0.0.0:27017/sports');
+mongoose.connect('mongodb://10.0.0.14/sports');
 var models_path = path.join(__dirname, './../models');
 fs.readdirSync(models_path).forEach(function(file) {
 	if(file.indexOf('.js') >= 0) {
